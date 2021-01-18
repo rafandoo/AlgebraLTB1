c17e926ab7acb94e0b344f1fb7afb61c2c072042
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-01-18 14:01:43 -0300

    update v1.9.3.3
    update criação métodos de pratos

--- filaRestaurante.java
+++ filaRestaurante.java
@@ -16,10 +16,55 @@ public class filaRestaurante {
 	
 	private int totalAlm;
 	
-	private int[] pratos;
+	//MOVER POSTERIOIRMENTE
+	private int[] p_prato;
 	private int primPrato;
 	private int ultPrato;
 	private int quantPrato;
+	
+	/*
+	//CONSTRUTOR DE PRATOS
+	public pratos() {
+		this.p_prato = new int[100];
+		this.primPrato = 0;
+		this.ultPrato = 0;
+		this.quantPrato = 0;
+	*/
+	
+	//GETTER E SETTER
+	public int getQuantPrato() {
+		return quantPrato;
+	}
+	public void setQuantPrato(int quantPrato) {
+		this.quantPrato = quantPrato;
+	}
+	
+	//METODO PARA INSERIR PRATOS
+	public void inserirPrato(int num) {
+		if(this.ehCheioPilha()) {
+			System.out.println("A pilha de pratos esta cheia!");
+		} else {
+			this.p_pratos [this.ultPratos] = num;
+			this.ultPratos = ((this.ultPratos + 1) % this.p_pratos.length);
+			this.quantPrato++;	
+		}
+	}
+	
+	//METODO PARA RETIRAR PRATOS
+	public int removePrato() {
+		if(this.ehVazioFR()) {
+			System.out.println("A fila esta vazia!");
+			return (0);
+		} else {
+			int aux = filaRest[this.primRest];
+			this.primRest = ((this.primRest + 1) % this.filaRest.length);
+			this.totalRest--;
+			this.totalAlm++;
+			return aux;
+		}
+	}
+	
+	
 	
 	//CONSTRUTOR
 	public filaRestaurante() {
